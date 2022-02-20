c4139e1eaf2c6bf520762a51b429f17ed0601fca
unknown <varra2@yandex.ru>
2022-02-20 20:21:20 +0300

    search by name

--- recipes_project/recipebook/views.py
+++ recipes_project/recipebook/views.py
@@ -4,6 +4,11 @@ from recipebook.models import Meal, Ingredient
 
 def index(request):
 
-    recipes = Meal.objects.all()
+    if request.method == "GET":
+        recipes = Meal.objects.all()
+    if request.method == "POST":
+        lookup_recipe = request.POST["lookup_recipe"].lower()
+        print(lookup_recipe)
+        recipes = Meal.objects.filter(name__icontains=lookup_recipe)
 
     return render(request, "recipebook/index.html", {"recipes": recipes})
