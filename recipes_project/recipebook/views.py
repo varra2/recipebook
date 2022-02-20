944018ae0379eae57896f610b7c56b5a11dd4b69
unknown <varra2@yandex.ru>
2022-02-20 21:22:24 +0300

    ingredient selection added

--- recipes_project/recipebook/views.py
+++ recipes_project/recipebook/views.py
@@ -7,8 +7,16 @@ def index(request):
     if request.method == "GET":
         recipes = Meal.objects.all()
     if request.method == "POST":
-        lookup_recipe = request.POST["lookup_recipe"].lower()
-        print(lookup_recipe)
-        recipes = Meal.objects.filter(name__icontains=lookup_recipe)
+        if "lookup_recipe" in request.POST:
+            lookup_recipe = request.POST["lookup_recipe"].lower()
+            recipes = Meal.objects.filter(name__icontains=lookup_recipe)
+        elif "lookup_ingredient" in request.POST:
+            lookup_ingredient = request.POST["lookup_ingredient"]
+            recipes = Meal.objects.filter(ingredients__in=[lookup_ingredient])
+    ingredients = Ingredient.objects.all()
 
-    return render(request, "recipebook/index.html", {"recipes": recipes})
+    return render(
+        request,
+        "recipebook/index.html",
+        {"recipes": recipes, "ingredients": ingredients},
+    )
