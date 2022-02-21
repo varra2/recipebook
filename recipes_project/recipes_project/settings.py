5d009fa586d16c361d24d1b64201c9670e1d9ba5
unknown <varra2@yandex.ru>
2022-02-21 16:32:49 +0300

    requirements added

--- recipes_project/recipes_project/settings.py
+++ recipes_project/recipes_project/settings.py
@@ -56,7 +56,6 @@ ROOT_URLCONF = "recipes_project.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        # "DIRS": [BASE_DIR / "templates"],
         "DIRS": [],
         "APP_DIRS": True,
         "OPTIONS": {
